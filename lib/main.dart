9fc49ca42bbf6a4382d610bafdf9e01804aae1d2
bernardoxm <bernardomelofacu@gmail.com>
2024-03-26 00:22:07 -0300

    v5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/order_list.dart';
 import 'package:shop/pages/cart_page.dart';
 
 import '/models/product_list.dart';
@@ -24,6 +25,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (_) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => OrderList(),
         ),
       ],
       child: MaterialApp(
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/cart_item.dart';
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/order_list.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -22,7 +23,7 @@ class CartPage extends StatelessWidget {
       ),
       body: Column(children: [
         Card(
-          margin: const EdgeInsets.symmetric(horizontal: 15,vertical: 25),
+          margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
           child: Padding(
             padding: const EdgeInsets.all(10.0),
             child: Row(
@@ -40,15 +41,32 @@ class CartPage extends StatelessWidget {
                 Chip(
                   label: Text(
                     'R\$${cart.totalAmount}',
-                    style: TextStyle(color: Colors.green),
+                    style: TextStyle(color: Colors.black, fontSize: 15),
                   ),
+                  backgroundColor: Colors.yellow,
                 ),
                 Spacer(),
                 TextButton(
-                  child: Text('Comprar'),
+                  child: Text(
+                    'Comprar',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),
+                  ),
                   style: TextButton.styleFrom(
-                      textStyle: TextStyle(color: Colors.black)),
-                  onPressed: () {},
+                    backgroundColor: Colors.yellow,
+                    primary: Colors.black, // Text color
+                    shape: RoundedRectangleBorder(
+                      
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () {
+                    Provider.of<OrderList>(context, listen: false)
+                        .addOrder(cart);
+                    cart.clear();
+                  },
                 ),
               ],
             ),
@@ -57,7 +75,8 @@ class CartPage extends StatelessWidget {
         Expanded(
           child: ListView.builder(
             itemCount: items.length,
-            itemBuilder: (context, index) => CartItemWidget(cartItem: items[index]),
+            itemBuilder: (context, index) =>
+                CartItemWidget(cartItem: items[index]),
           ),
         ),
       ]),
